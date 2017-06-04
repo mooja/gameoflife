38ae3d34a04a505a65ebc514dc0bfae919d47d4c
Max Shkurygin <max.atreides@gmail.com>
2017-06-04 08:36:39 -0400

    added uglyfy and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var tsify = require("tsify");
-var watchify = require("watchify");
-var gutil = require("gulp-util");
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var paths = {
     pages: ['src/*.html']
-}
+};
 
+gulp.task('copyHtml', function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest('dist'));
+});
 
-var watchedBrowserify = watchify(browserify({
+gulp.task('default', ['copyHtml'], function () {
+    return browserify({
         basedir: '.',
         debug: true,
-        entries: ["src/main.ts"],
+        entries: ['src/main.ts'],
         cache: {},
         packageCache: {}
-}).plugin(tsify));
-
-
-gulp.task("copy-html", function() {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
-})
-
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(gulp.dest("dist"));
-}
-
-gulp.task("default", ["copy-html"], bundle);
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", gutil.log);
+    })
+    .plugin(tsify)
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
