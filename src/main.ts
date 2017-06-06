c24e78021c548c30671ae91afb2723188ce9bcf9
Max Shkurygin <max.atreides@gmail.com>
2017-06-06 08:57:12 -0400

    removed external calls since this is a very simple program

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import { sayHello } from "./greet";
-
-
 function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
-    elt.innerText = sayHello(name);
+    elt.innerText = `Hello ${name}`;
 }
 
 showHello("greeting", "TypeScript");
