4340b94ff4ecc802d77c79650273d874d0702b4d
Max Shkurygin <max.atreides@gmail.com>
2017-06-06 08:55:27 -0400

    greatly simplified the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,34 @@
+"use strict;"
+
 var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var tsify = require("tsify");
-var sass = require("gulp-sass")
-var watchify = require("watchify");
-var gutil = require("gulp-util");
-var paths = {
-    pages: ['src/*.html']
-}
+var sass = require("gulp-sass");
+var ts = require("gulp-typescript");
 
-
-var watchedBrowserify = watchify(browserify({
-        basedir: '.',
-        debug: true,
-        entries: ["src/main.ts"],
-        cache: {},
-        packageCache: {}
-}).plugin(tsify));
-
-gulp.task("copy-html", function() {
-    return gulp.src(paths.pages)
+gulp.task("html", function() {
+    return gulp.src("src/*.html")
         .pipe(gulp.dest("dist"));
 })
 
 gulp.task("sass", function() {
-  return gulp.src("./src/*.scss")
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task("sass:watch", function() {
-  gulp.watch("./src/*.scss", ["watch"]);
-});
-
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .pipe(source("bundle.js"))
+    return gulp.src("src/*.scss")
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest("dist"));
-}
+})
 
-gulp.task("default", ["copy-html", "sass"], bundle);
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", gutil.log);
+gulp.task("typescript", function() {
+    return gulp.src("src/main.ts")
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: "main.js"
+        }))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["html", "typescript", "sass"]);
+
+gulp.task("watch", function() {
+    var watcher = gulp.watch("src/*", ["typescript", "html", "sass"]);
+    watcher.on("change", function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    })
+});
