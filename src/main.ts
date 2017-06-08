2e2e1b291382b5da773f99159e70fa03df0fc84c
Max Shkurygin <max.atreides@gmail.com>
2017-06-08 08:31:55 -0400

    simple GoL datamodel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = `Hello ${name}`;
+interface Pos {
+    x: number;
+    y: number;
 }
 
-showHello("greeting", "TypeScript");
+class ConwayGrid {
+    alivePositions: Pos[];
+    height: number;
+    width: number;
+
+    constructor(width: number, height: number, positions: Pos[]) {
+        this.height = height;
+        this.width = width;
+        this.alivePositions = positions;
+    }
+
+    toString() {
+        for(let row = 0; row < this.height; row++) {
+            const aliveOnThisRow: Pos[] = this.alivePositions.filter(pos => pos.x === row);
+            let lineStr: string = '';
+            for(let col = 0; col < this.width; col++) {
+                const blockIsAlive: boolean = aliveOnThisRow.some(pos => pos.y === col);
+                lineStr +=  blockIsAlive ? '#' : ' ';
+            }
+            console.log(lineStr+'\n');
+        }
+    }
+}
+
+let g = new ConwayGrid(5, 5, [{x:1, y:1}, {x:2, y:2}, {x: 4, y:4}]);
+console.log(g.toString());
