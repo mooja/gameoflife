16e7da78e2dfb725807513e1980fcb5d20f9012b
Max Shkurygin <max.atreides@gmail.com>
2017-06-08 09:11:18 -0400

    added neighbours methods

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,19 @@ class ConwayGrid {
         this.alivePositions = positions;
     }
 
+    getNeighbours(target: Pos): Pos[] {
+        return this.alivePositions.filter(p => {
+            if (Math.abs(target.x - p.x) > 1) return false;
+            if (Math.abs(target.y - p.y) > 1) return false;
+            if (target.x === p.x && target.y === p.y) return false;
+            return true;
+        });
+    }
+
+    getNumNeighbours(target: Pos): number {
+        return this.getNeighbours(target).length;
+    }
+
     toString(): string {
         let rv: string = '';
         for(let row = 0; row < this.height; row++) {
@@ -33,7 +46,9 @@ class ConwayGrid {
     }
 }
 
-const g = new ConwayGrid(5, 5, [{x:1, y:1}, {x:2, y:2}, {x: 4, y:4}]);
+const g = new ConwayGrid(5, 5, [{x:1, y:1}, {x:2, y:2}, {x: 3, y:3}]);
 const c: Element = document.querySelector("canvas.grid");
 
 console.log(g.toString());
+console.log(g.getNeighbours({x:2, y: 2}));
+console.log(g.getNumNeighbours({x:2, y: 2}));
