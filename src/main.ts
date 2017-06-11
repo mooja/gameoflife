7dd81c9d88f264d85293491b058d57e5af41d0cf
Max Shkurygin <max.atreides@gmail.com>
2017-06-10 23:57:19 -0400

    simple slider

--- src/main.ts
+++ src/main.ts
@@ -87,7 +87,7 @@ class CanvasDrawer {
     }
 }
 
-const g = new ConwayGrid(5, 5, [{x:0, y:0}, {x:0, y:1}, {x:0, y:2}]);
+const g = new ConwayGrid(5, 5, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
 const c: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
 const drawer = new CanvasDrawer(g, c);
 
