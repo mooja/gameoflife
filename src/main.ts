b0fc7c970a0f3b30222b26cc1d5a8bdb8e6f4cd1
Max Shkurygin <max.atreides@gmail.com>
2017-06-13 15:21:02 -0400

    added a pause/resume button

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
 import { ConwayGrid, Pos } from './ConwayGrid';
 import { CanvasDrawer } from './CanvasDrawer';
 
-const g = new ConwayGrid(20, 20, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
-const c: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
-const drawer = new CanvasDrawer(g, c);
+function main(): void {
+    const g = new ConwayGrid(20, 20, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
+    const c: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
+    const drawer = new CanvasDrawer(g, c);
 
-setInterval(() => {
-    drawer.draw();
-    g.next()
-},
-400);
+    let gameIsPaused = false;
+    let gameIntervalId: number = runGame(g, drawer);
+    function runGame(g: ConwayGrid, drawer: CanvasDrawer): number {
+        return window.setInterval(() => {
+            drawer.draw();
+            g.next()
+        }, 400);
+    }
+
+    const pauseButton = document.querySelector(".pause_button");
+    pauseButton.addEventListener("click", event => {
+        event.preventDefault();
+        if (gameIsPaused) {
+            gameIntervalId = runGame(g, drawer);
+        }
+        else {
+            window.clearInterval(gameIntervalId);
+        }
+        gameIsPaused = !gameIsPaused;
+    });
+
+    console.log('hi');
+}
+
+window.addEventListener("load", main);
