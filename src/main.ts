37d0e518f0ad7a091eb241b8139e5f4141589c3e
Max Shkurygin <max.atreides@gmail.com>
2017-06-14 08:30:56 -0400

    added pause and next step functionality

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,41 @@ function main(): void {
     const g = new ConwayGrid(20, 20, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
     const c: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
     const drawer = new CanvasDrawer(g, c);
+    drawer.draw();
+
+    const nextStepButton = document.querySelector(".next_step");
+    const pauseButton = document.querySelector(".pause_button");
 
     let gameIsPaused = false;
     let gameIntervalId: number = runGame(g, drawer);
     function runGame(g: ConwayGrid, drawer: CanvasDrawer): number {
         return window.setInterval(() => {
-            drawer.draw();
             g.next()
+            drawer.draw();
         }, 400);
     }
+    nextStepButton.setAttribute("disabled", "true");
 
-    const pauseButton = document.querySelector(".pause_button");
     pauseButton.addEventListener("click", event => {
         event.preventDefault();
         if (gameIsPaused) {
             gameIntervalId = runGame(g, drawer);
+            pauseButton.setAttribute("value", "Pause");
+            nextStepButton.setAttribute("disabled", "true");
         }
         else {
             window.clearInterval(gameIntervalId);
+            pauseButton.setAttribute("value", "Resume");
+            nextStepButton.removeAttribute("disabled");
         }
         gameIsPaused = !gameIsPaused;
     });
 
-    console.log('hi');
+    nextStepButton.addEventListener("click", event => {
+        if (!gameIsPaused) return;
+        g.next();
+        drawer.draw();
+    });
 }
 
 window.addEventListener("load", main);
