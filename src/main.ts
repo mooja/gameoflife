7935704528d3916b49be82a2e94e5a11560ff821
Max Shkurygin <max.atreides@gmail.com>
2017-06-16 15:12:30 -0400

    added simple styles

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ConwayGrid, Pos } from './ConwayGrid';
 import { CanvasDrawer } from './CanvasDrawer';
 
 function main(): void {
-    const g = new ConwayGrid(20, 20, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
+    const g = new ConwayGrid(40, 40, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
     const c: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
     const drawer = new CanvasDrawer(g, c);
     drawer.draw();
