abff46308c936f73b03a73c2d51dcb35c886d8d5
Max Shkurygin <max.atreides@gmail.com>
2017-06-21 04:23:13 -0400

    slight refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,17 @@ import { ConwayGrid, Pos } from './ConwayGrid';
 import { CanvasDrawer } from './CanvasDrawer';
 
 function main(): void {
-    const g = new ConwayGrid(20, 20, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
-    const c: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
-    const drawer = new CanvasDrawer(g, c);
-    drawer.draw();
+    const grid = new ConwayGrid(20, 20, [{x:1, y:0}, {x:2, y:1}, {x:2, y:2}, {x:1, y:2}, {x:0, y:2}]);
+    const canvas: HTMLCanvasElement  = <HTMLCanvasElement>document.querySelector("canvas.grid");
+    const renderer: CanvasDrawer = new CanvasDrawer(grid, canvas);
+    renderer.draw();
 
-    const pauseButton = document.querySelector(".pause_button");
-    const nextStepButton = document.querySelector(".next_step_button");
+    const pauseButton: Element = document.querySelector(".pause_button");
+    const nextStepButton: Element = document.querySelector(".next_step_button");
+    const rangeControl: Element = document.querySelector("speed");
 
     let gameIsPaused = false;
-    let gameIntervalId: number = runGame(g, drawer);
+    let gameIntervalId: number = runGame(grid, renderer);
     function runGame(g: ConwayGrid, drawer: CanvasDrawer): number {
         return window.setInterval(() => {
             g.next()
@@ -19,9 +20,9 @@ function main(): void {
         }, 400);
     }
 
-    pauseButton.addEventListener("click", event => {
+    function handlePauseButton(event) {
         if (gameIsPaused) {
-            gameIntervalId = runGame(g, drawer);
+            gameIntervalId = runGame(grid, renderer);
             pauseButton.innerHTML = "Pause";
         }
         else {
@@ -30,24 +31,25 @@ function main(): void {
             nextStepButton.removeAttribute("disabled");
         }
         gameIsPaused = !gameIsPaused;
-    });
+    }
+    pauseButton.addEventListener("click", handlePauseButton);
 
-    nextStepButton.addEventListener("click", event => {
+    function handleStepButton(event) {
         if (!gameIsPaused) return;
-        g.next();
-        drawer.draw();
-    });
+        grid.next();
+        renderer.draw();
+    }
+    nextStepButton.addEventListener("click", handleStepButton);
 
-    c.addEventListener('click', e => {
-        const x = e.clientX - c.offsetLeft;
-        const y = e.clientY - c.offsetTop;
-        const [gridX, gridY] = drawer.getGridPos(x, y);
-
-        if(gameIsPaused) {
-            g.toggleCell({x: gridX, y: gridY});
-            drawer.draw();
-        }
-    });
+    function handleCanvasClick(event) {
+        if(!gameIsPaused) return;
+        const x = event.clientX - canvas.offsetLeft;
+        const y = event.clientY - canvas.offsetTop;
+        const [gridX, gridY] = renderer.getGridPos(x, y);
+        grid.toggleCell({x: gridX, y: gridY});
+        renderer.draw();
+    }
+    canvas.addEventListener('click', handleCanvasClick);
 }
 
 window.addEventListener("load", main);
